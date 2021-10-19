58d8a3ca2dd79da958b5c42ce17a3376ed4bfc06
marsellino10 <54001585+marsellino10@users.noreply.github.com>
2021-10-19 13:59:25 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -18,16 +18,37 @@ const topBtn = document.getElementById('top-btn');
 	@para        : none
 */
 function createNewSection() {
+	//increment the counter of sections
 	lastSection++;
-	const newSection = `<section id="section${lastSection}" data-nav="Section ${lastSection}">
-    <div class="landing__container">
-    <h2>Section ${lastSection}</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
-    
-    <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
-    </div>
-    </section>`;
-    mainSection.insertAdjacentHTML("beforeend", newSection);
+
+	//create a new section
+	let section = document.createElement("section");
+
+	//set attributes (id and data-nav) to the created section
+	section.setAttribute("id",`section${lastSection}`);
+	section.setAttribute("data-nav",`section ${lastSection}`);
+
+	//create div and set the class atrribute with the landing__container class
+	let div = document.createElement("div");
+	div.setAttribute("class","landing__container");
+
+	//create h2 header and write the name of the section in it
+	let h2 = document.createElement("h2");
+	h2.textContent = `section ${lastSection}`;
+
+	//create a paragraph and write in it any thing
+	let p = document.createElement("p");
+	p.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.";
+
+	//append in div multiple elements(h2 and p)
+	div.append(h2,p);
+
+	//append the whole div to the created section
+	section.append(div);
+
+	//and finally append the section to the main in html 
+	mainSection.append(section);
+
 }
 
 /*
@@ -36,13 +57,32 @@ function createNewSection() {
  	@para        : none
 */
 function createNavItem(){
+	//make the nav menu empty to avoid repeating of the sections every time adding new section
      navMenu.innerHTML = '';
+
+     //the variable sections has all section's tag
      const sections = document.querySelectorAll('section');
 
+     //iterate over all sections to add them to the navigation menu as a new list item 
      sections.forEach(function(section){
           const sectionName = section.getAttribute('data-nav');
-          const listItem = `<li><a href="#${section.id}" data-nav="${section.id}" class="menu__link">${sectionName}</a></li>`;
-          navMenu.insertAdjacentHTML("beforeend",listItem);
+          
+          //create a new list item
+          let item = document.createElement("li");
+
+          //create a link to the place of the section in the page
+          const link = document.createElement("a");
+          link.setAttribute("href",`#${section.id}`);
+          link.setAttribute("data-nav",`${section.id}`);
+          link.setAttribute("class","menu__link");
+          link.innerHTML = sectionName;
+
+          //append the link with the name of the section to the list item
+          item.append(link);
+
+          //finally append the list item to the navigation menu
+          navMenu.append(item);
+
      });
 }
 
@@ -54,10 +94,14 @@ function createNavItem(){
 function randomImages(){
 	let image = document.querySelector('.main__hero');
 
+    //array with the name of images
 	let imagesArr = ["1.jpg","2.jpg"];
+
+	//choose random photo to apeear every 5 seconds
   setInterval(function(){
   	let randomNum = Math.floor(Math.random() * imagesArr.length);
 
+  	//style the photo dynamically with js
   	image.style.cssText = 'object-fit: fill; obacity: 0.5; background-size: 100% 100%; background-repeat:no-repeat;';
 	image.style.backgroundImage = 'url("jpg/' + imagesArr[randomNum] + '")';
      },5000);
@@ -89,12 +133,16 @@ document.getElementById("btn").addEventListener("click", function(){
 });
 
 //add event: when the top button is clicked move the viewport the top again
-topBtn.addEventListener("click",function(){window.scrollTo(0,0)});
+topBtn.addEventListener("click",function(){
+	window.scrollTo(0,0)
+});
 
 
 //add the active class to the active section in the viewport 
 //add top button
 window.onscroll = function() {
+
+	//first part to add the active class to the active section
 	document.querySelectorAll("section").forEach(function(active) {
 		
 	if(active.getBoundingClientRect().top >= -450 && active.getBoundingClientRect().top <= 100){
@@ -105,6 +153,8 @@ window.onscroll = function() {
         }
 	}
     );
+
+    //second part to display the top button after scrolling 800px or more and hide it otherwise
     if(window.scrollY >= 800){
     	topBtn.style.display = 'block';
     }
@@ -120,3 +170,4 @@ window.onscroll = function() {
                 //==========================> start of the program  <===========================//
 
 start();
+
